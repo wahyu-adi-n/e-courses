f6a2440281a864a34b028119597b411fb4a85650
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmai.com>
2023-07-15 21:15:45 +0700

    delete profile and settings section

--- app/Views/admin/edit_pelatihan_page.php
+++ app/Views/admin/edit_pelatihan_page.php
@@ -107,16 +107,17 @@
                                     <br><?= session()->get("email"); ?></span>
                                 <!-- <img class="img-profile rounded-circle" src="img/undraw_profile.svg"> -->
                             </a>
+
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
+                                <!-- <a class="dropdown-item" href="#">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Settings
-                                </a>
+                                </a> -->
 
                                 <div class="dropdown-divider"></div>
                                 <form action="/logout" method="post" class="user">
