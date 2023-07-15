2cf5648eee48937cbb4fed09f2acca4c168512b1
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmai.com>
2023-07-15 20:35:52 +0700

    delete user.sql and add courses.sql

---
+++ courses.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Jul 2023 pada 15.35
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `courses`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `instruktur`
+--
+
+CREATE TABLE `instruktur` (
+  `kode_instruktur` int(11) NOT NULL,
+  `nama_instruktur` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `nohp` varchar(13) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `instruktur`
+--
+
+INSERT INTO `instruktur` (`kode_instruktur`, `nama_instruktur`, `alamat`, `nohp`, `email`) VALUES
+(2, 'Budi Karya Sumadi', 'Salatiga', '081890098765', 'budi@gmail.com'),
+(3, 'Akbar Galang Wijaya', 'Asinan', '08828766798', 'akbar@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelatihan`
+--
+
+CREATE TABLE `pelatihan` (
+  `kode_pelatihan` int(11) NOT NULL,
+  `nama_pelatihan` varchar(100) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `tgl_mulai` date NOT NULL,
+  `tgl_selesai` date NOT NULL,
+  `lokasi` varchar(50) NOT NULL,
+  `kode_instruktur` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pelatihan`
+--
+
+INSERT INTO `pelatihan` (`kode_pelatihan`, `nama_pelatihan`, `deskripsi`, `tgl_mulai`, `tgl_selesai`, `lokasi`, `kode_instruktur`) VALUES
+(2, 'AI For Junior Developer', 'Pelatihan ini berisi materi seputar kecerdasan buatan dengan dibawakan oleh instruktur yang ahli di bidangnya.', '2023-07-01', '2023-07-22', 'Semarang, Offline', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `kode_user` int(15) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`kode_user`, `nama`, `username`, `email`, `password`) VALUES
+(1, 'Wahyu Adi Nugroho', 'wahyu.adi.nugroho', 'wahyuadinugroho@gmail.com', '12345678'),
+(8, 'Nanang Wahid', 'nanang.wahid', 'nanangwahid@gmail.com', '12345678');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `instruktur`
+--
+ALTER TABLE `instruktur`
+  ADD PRIMARY KEY (`kode_instruktur`);
+
+--
+-- Indeks untuk tabel `pelatihan`
+--
+ALTER TABLE `pelatihan`
+  ADD PRIMARY KEY (`kode_pelatihan`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`kode_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `instruktur`
+--
+ALTER TABLE `instruktur`
+  MODIFY `kode_instruktur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `pelatihan`
+--
+ALTER TABLE `pelatihan`
+  MODIFY `kode_pelatihan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `kode_user` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
